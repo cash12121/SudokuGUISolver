0687b3974c9b0dd07da5b680646978048ea19e0a
Cash Carlson <cash12121@gmail.com>
2020-10-31 22:44:03 -0500

    Initial commit

---
+++ LogicLayer/SudokuManager.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLayer
+{
+	public class SudokuManager
+	{
+		public int[,] SudokuBoard { get; set; }
+
+		public SudokuManager()
+		{
+			SetBoard();
+			EmptyBoard();
+		}
+
+		public SudokuManager(int[,] intialBoard)
+		{
+			SetBoard();
+			UpdatedBoard(intialBoard);
+		}
+
+		public void SetBoard()
+		{
+			SudokuBoard = new int[9, 9];
+		}
+
+
+		public void EmptyBoard()
+		{
+			for (int i = 0; i < SudokuBoard.GetLength(0); i++)
+			{
+				for (int j = 0; j < SudokuBoard.GetLength(1); j++)
+				{
+					SudokuBoard[i, j] = 0;
+				}
+			}
+		}
+
+		public void UpdatedBoard(int[,] updatedBoard)
+		{
+			for (int i = 0; i < SudokuBoard.GetLength(0); i++)
+			{
+				for (int j = 0; j < SudokuBoard.GetLength(1); j++)
+				{
+					SudokuBoard[i, j] = updatedBoard[i,j];
+				}
+			}
+		}
+
+		public bool CheckRow(int row)
+		{
+			bool isValid = true;
+			HashSet<int> dupes = new HashSet<int>();
+
+			for (int col = 0; col < SudokuBoard.GetLength(0); col++)
+			{
+				if (dupes.Add(SudokuBoard[row,col]) == false)
+				{
+					isValid = false;
+				}
+			}
+
+			return isValid;
+		}
+
+		public bool CheckColumn(int col)
+		{
+			bool isValid = true;
+			HashSet<int> dupes = new HashSet<int>();
+
+			for (int row = 0; row < SudokuBoard.GetLength(0); row++)
+			{
+				if (dupes.Add(SudokuBoard[row, col]) == false)
+				{
+					isValid = false;
+				}
+			}
+
+			return isValid;
+		}
+
+		public bool CheckBox(int row, int col)
+		{
+			bool isValid = true;
+			HashSet<int> dupes = new HashSet<int>();
+
+			row = (row / 3) * 3;
+			col = (col / 3) * 3;
+
+			for (int i = 0; i < 3; i++)
+			{
+				for (int j = 0; j < 3; j++)
+				{
+					if (dupes.Add(SudokuBoard[row + i, col + j]) == false)
+					{
+						isValid = false;
+					}
+				}
+			}
+
+			return isValid;
+		}
+
+		public bool Possible(int row, int col, int num)
+		{
+			//bool isValid = true;
+
+			// Check Row
+			for (int i = 0; i < SudokuBoard.GetLength(0); i++)
+			{
+				if (SudokuBoard[row,i] == num)
+				{
+					return false;
+				}
+			}
+
+			// Check Column
+			for (int i = 0; i < SudokuBoard.GetLength(1); i++)
+			{
+				if (SudokuBoard[i, col] == num)
+				{
+					return false;
+				}
+			}
+
+			// Floor Division
+
+			row = (row / 3) * 3;
+			col = (col / 3) * 3;
+
+			for (int i = 0; i < 3; i++)
+			{
+				for (int j = 0; j < 3; j++)
+				{
+					if (SudokuBoard[row + i, col + j] == num)
+					{
+						return false;
+					}
+				}
+			}
+
+			return true;
+		}
+
+		/// <summary>
+		/// Method for solving the board and returning that if the board is solved or not.
+		/// </summary>
+		/// <returns>bool Returns if the board was solved.</returns>
+		public bool SolveBoard()
+		{
+			// Loop through row
+			for (int x = 0; x < 9; x++)
+			{
+				// Loop through column
+				for (int y = 0; y < 9; y++)
+				{
+					// Check if the selected spot is equal to 0
+					if (SudokuBoard[x,y] == 0)
+					{
+						// Try looping through numbers 1 through 9
+						for (int num = 1; num < 10; num++)
+						{
+							//Check if number is possible to add to spot
+							if (Possible(x, y, num))
+							{
+								SudokuBoard[x, y] = num;
+
+								// Check if it is a good number
+								if (SolveBoard())
+								{
+									return true;
+								}
+
+								// If the number is not the apart of the solution reset number to 0
+								SudokuBoard[x, y] = 0;
+							}
+						}
+
+						// Return false if 1-9 cannot be the solution fo the spot
+						// and return to previous spot via backtracking
+						return false;
+					}
+					
+				}
+			}
+
+			// Once this spot is reached it means have reached the end of the 2D
+			// Array and the last number is correct. Therefore return true.
+			return true;
+		}
+	}
+}
---
+++ PresentationLayer/MainWindow.xaml.cs
@@ -0,0 +1,146 @@
+﻿using LogicLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PresentationLayer
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private SudokuManager sudokuManager;
+		private TextBox[,] txtGrid = new TextBox[9, 9];
+
+		public MainWindow()
+		{
+			sudokuManager = new SudokuManager();
+			InitializeComponent();
+		}
+
+		/// <summary>
+		/// Method for executing on window load.
+		/// Fills in the uniform grid with TextBoxes to use for Sudoku.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+
+			for (int i = 0; i < txtGrid.GetLength(0); i++)
+			{
+				for (int j = 0; j < txtGrid.GetLength(1); j++)
+				{
+					// Add new TextBoxes
+					txtGrid[i, j] = new TextBox();
+					ugaBoard.Children.Add(txtGrid[i, j]);
+					ChangeText(i, j);
+
+					// Add Attriubutes
+					txtGrid[i, j].TextChanged += new TextChangedEventHandler(TextBox_TextChanged);
+					txtGrid[i, j].MaxLength = 1;
+					txtGrid[i, j].MaxLines = 1;
+					txtGrid[i, j].FontSize = 16;
+					txtGrid[i, j].TextAlignment = TextAlignment.Center;
+
+					// Add Borders
+					txtGrid[i, j].BorderBrush = Brushes.Black;
+					Thickness setThickness = new Thickness(.5, .5, .5, .5);
+
+					//Check Boundaries
+					//Check Top
+					if (i == 0)
+					{
+						setThickness.Top = 2;
+					}
+
+					// Check Bottom
+					if (i == 8)
+					{
+						setThickness.Bottom = 2;
+					}
+
+					// Check Left
+					if (j == 0)
+					{
+						setThickness.Left = 2;
+					}
+
+					// Check Right
+					if (j == 8)
+					{
+						setThickness.Right = 2;
+					}
+
+					//Check and add Dividers
+
+					if (j == 2 || j == 5)
+					{
+						setThickness.Right = 2;
+					}
+
+					if (i == 2 || i == 5)
+					{
+						setThickness.Bottom = 2;
+					}
+
+					//Apply Border Thickness
+					txtGrid[i, j].BorderThickness = setThickness;
+				}
+			}
+
+			Print2DArray<int>(sudokuManager.SudokuBoard);
+			sudokuManager.SolveBoard();
+			Console.WriteLine("\nSolved Board\n");
+			Print2DArray<int>(sudokuManager.SudokuBoard);
+		}
+
+		private void ChangeText(int i, int j)
+		{
+			if(sudokuManager.SudokuBoard[i,j].ToString().Equals("0"))
+			{
+				txtGrid[i, j].Text = "";
+			} else
+			{
+				txtGrid[i, j].Text = sudokuManager.SudokuBoard[i, j].ToString();
+			}
+		}
+
+		public static void Print2DArray<T>(T[,] matrix)
+		{
+			for (int i = 0; i < matrix.GetLength(0); i++)
+			{
+				for (int j = 0; j < matrix.GetLength(1); j++)
+				{
+					Console.Write(matrix[i, j] + "\t");
+				}
+				Console.WriteLine();
+			}
+		}
+
+		private void btnReset_Click(object sender, RoutedEventArgs e)
+		{
+			//TODO Finsh Method
+			Console.WriteLine("Click");
+		}
+
+		private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			//TODO Finsh Method
+			Console.WriteLine("Changed");
+		}
+	}
+}
